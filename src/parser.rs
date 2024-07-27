3e862274d6e2a0ae23bb480c3ba414a0912b19e0
Alex Westerman <alex@faceftw.dev>
2024-07-27 17:17:01 -0400

    Begin implementing Parsers

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,19 @@
-// pub struct TerminalColor {
-//     red: u8,
-//     green: u8,
-//     blue: u8,
-// }
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::{digit1, newline},
+    combinator::map,
+    number::complete::u8,
+    sequence::{delimited, preceded, terminated, tuple},
+    IResult,
+};
 
 //For lack of a better name
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum TerminalCharacter {
     Space,
-    TerminalEscape(),
+    DefaultForegroundColor,
+    DefaultBackgroundColor,
     TerminalForegroundColor8(u8),
     TerminalForegroundColor24(u32),
     TerminalBackgroundColor8(u8),
@@ -16,4 +21,42 @@ pub enum TerminalCharacter {
     UnicodeCharacter(char),
     ThoughtPlaceholder,
     Newline,
+    Comment,
+}
+
+fn terminal_chars(input: &str) -> IResult<&str, TerminalCharacter> {
+    alt((
+        map(
+            delimited(tag("\\e["), digit1, tag("m")),
+            |esc: &str| match esc {
+                "39" => Ok(TerminalCharacter::DefaultForegroundColor),
+                "49" => Ok(TerminalCharacter::DefaultBackgroundColor),
+                _ => Err("Unknown Escape!"),
+            },
+        ),
+        // map(
+        //     delimited(
+        //         tag("\\e["),
+        //         tuple(digit1, tag(";"), digit1, tag(";"), digit1),
+        //         tag("m"),
+        //     ),
+        //     |esc: &str| todo!(),
+        // ),
+    ))(input);
+    todo!()
+}
+
+fn unicode_char(input: &str) -> IResult<&str, TerminalCharacter> {
+    todo!()
+}
+
+fn spaces_and_lines(input: &str) -> IResult<&str, TerminalCharacter> {
+    alt((
+        map(tag("\n"), |_| TerminalCharacter::Newline),
+        map(tag(" "), |_| TerminalCharacter::Space),
+    ))(input)
+}
+
+fn terminal_foreground_8(input: &str) -> IResult<&str, TerminalCharacter> {
+    todo!()
 }
