20e3b65da87a9190ba15a8f78fbd7370dfef3d40
Alex Westerman <alex@faceftw.dev>
2024-08-31 19:25:23 -0400

    General Cleanup Related to Conditional Compilation

--- src/fortune.rs
+++ src/fortune.rs
@@ -1,6 +1,6 @@
+use std::error::Error;
+#[cfg(not(feature = "inline-fortune"))]
 use std::{
-    cell::LazyCell,
-    error::Error,
     ffi::OsStr,
     fs::{self, File},
     io::{self, Read},
@@ -8,7 +8,8 @@ use std::{
 };
 use tinyrand::Rand;
 
-//default method of getting a fortune, without using the index file.
+///default method of getting a fortune, without using the index file.
+#[cfg(not(feature = "inline-fortune"))]
 pub fn get_fortune(file_path: PathBuf, rng: &mut impl Rand) -> Result<String, Box<dyn Error>> {
     match File::open(file_path) {
         Ok(mut file) => {
@@ -21,13 +22,98 @@ pub fn get_fortune(file_path: PathBuf, rng: &mut impl Rand) -> Result<String, Bo
         Err(e) => panic!("Could not open Fortune file! {e}"),
     }
 }
-// #[cfg(feature = "inline-fortune")]
-// const INLINE_FORTUNES: &'static str = include_str!("../target/resources/fortunes");
-// #[cfg(all(feature = "inline-fortune", feature = "inline-off-fortune"))]
-// const OFF_FORTUNES: &'static str = include_str!("../target/resources/off_fortunes");
-// #[cfg(all(feature = "inline", not(feature = "inline-off")))]
-// const OFF_FORTUNES: &'static str = "";
 
+#[cfg(not(feature = "inline-fortune"))]
+pub fn choose_fortune_file(
+    include_offensive: bool,
+    rng: &mut impl Rand,
+    supplied_path: Option<String>,
+) -> PathBuf {
+    let os = std::env::consts::OS;
+    if let Some(val) = supplied_path {
+        match fs::metadata(&val).unwrap().is_dir() {
+            true => choose_random_file(&PathBuf::from(val), include_offensive, rng)
+                .expect("Could not read the specified directory for getting fortunes"),
+            false => PathBuf::from(val),
+        }
+    } else if let Ok(val) = std::env::var("FORTUNE_FILE") {
+        PathBuf::from(val.as_str())
+    } else if let Ok(val) = std::env::var("FORTUNE_PATH") {
+        choose_random_file(&PathBuf::from(val.as_str()), include_offensive, rng)
+            .expect("Could not choose a random fortune file from the specified Fortune Path")
+    } else {
+        match os{
+            "linux" => choose_fortune_file(include_offensive, rng,Some(String::from("/usr/share/games/fortunes"))),
+            _ => panic!("I don't know what the default path for fortunes are for this OS!.\nPlease provide a FORTUNEPATH or FORTUNE_PATH environment variable, or a single file with FORTUNE_FILE")
+        }
+    }
+}
+
+#[cfg(not(feature = "inline-fortune"))]
+macro_rules! illegal_file_suffixes {
+    ($($ext:literal),*) => {
+        [
+            $(std::ffi::OsStr::new($ext)),*
+        ]
+    };
+}
+
+#[cfg(not(feature = "inline-fortune"))]
+pub fn choose_random_file(
+    path: &PathBuf,
+    include_offensive: bool,
+    rng: &mut impl Rand,
+) -> Result<PathBuf, io::Error> {
+    //Couple of rules when it comes to fortune files (defaults)
+    //1. Default fortunes are in /usr/share/games/fortunes
+    //2. Offensive fortunes are in /usr/share/games/fortunes/off. There is no prefix
+    //3. Currently, we load the entire text into memory, so we don't care about DAT
+    //4. Ignore Illegal suffixes in the ILLEGAL_SUFFIXES list
+
+    fn iterate(path: &PathBuf, include_offensive: bool) -> Vec<PathBuf> {
+        let illegal_file_suffixes: [&OsStr; 16] = illegal_file_suffixes!(
+            "dat", "pos", "c", "h", "p", "i", "f", "pas", "ftn", "ins.c", "ins.pas", "ins.ftn",
+            "sml", "sh", "pl", "csv"
+        );
+        let mut total_list = vec![];
+        let dir_list = fs::read_dir(path).expect("Could not open directory");
+        for entry in dir_list.filter(|item| {
+            !illegal_file_suffixes.contains(
+                &item
+                    .as_ref()
+                    .unwrap()
+                    .path()
+                    .extension()
+                    .unwrap_or_default(),
+            )
+        }) {
+            match entry {
+                Ok(item) => match item.metadata().unwrap().is_dir() {
+                    true => {
+                        if (item.file_name() != "./off")
+                            || (item.file_name() == "./off" && include_offensive)
+                        {
+                            total_list.append(&mut iterate(&item.path(), include_offensive))
+                        }
+                    }
+                    false => total_list.push(item.path()),
+                },
+                Err(e) => panic!("Could not identify a file in the fortune directory {e}"),
+            }
+        }
+        total_list
+    }
+
+    // let mut rng = thread_rng();
+    let list = iterate(path, include_offensive);
+    let chosen_idx = rng.next_lim_usize(list.len());
+
+    Ok(list[chosen_idx].clone())
+}
+
+/************************************************/
+/************Inline Feature Functions************/
+/************************************************/
 #[cfg(feature = "inline-fortune")]
 include!("../target/generated_sources/fortune_db.rs");
 
@@ -59,97 +145,4 @@ pub fn get_inline_fortune(
             choose_inline_fortune!(rng, FORTUNE_LIST)
         }
     }
-}
-
-pub fn choose_fortune_file(
-    include_offensive: bool,
-    rng: &mut impl Rand,
-    supplied_path: Option<String>,
-) -> PathBuf {
-    let os = std::env::consts::OS;
-    if let Some(val) = supplied_path {
-        match fs::metadata(&val).unwrap().is_dir() {
-            true => choose_random_fortune_file(&PathBuf::from(val), include_offensive, rng)
-                .expect("Could not read the specified directory for getting fortunes"),
-            false => PathBuf::from(val),
-        }
-    } else if let Ok(val) = std::env::var("FORTUNE_FILE") {
-        PathBuf::from(val.as_str())
-    } else if let Ok(val) = std::env::var("FORTUNE_PATH") {
-        choose_random_fortune_file(&PathBuf::from(val.as_str()), include_offensive, rng)
-            .expect("Could not choose a random fortune file from the specified Fortune Path")
-    } else {
-        match os{
-            "linux" => choose_fortune_file(include_offensive, rng,Some(String::from("/usr/share/games/fortunes"))),
-            _ => panic!("I don't know what the default path for fortunes are for this OS!.\nPlease provide a FORTUNEPATH or FORTUNE_PATH environment variable, or a single file with FORTUNE_FILE")
-        }
-    }
-}
-
-//Used LazyCell because We use this in an OsStr comparison context
-//Adds effectively an O(1) operation based on my understanding
-pub const ILLEGAL_FILE_SUFFIXES: LazyCell<[&OsStr; 13]> = LazyCell::new(|| {
-    [
-        OsStr::new("dat"),
-        OsStr::new("pos"),
-        OsStr::new("c"),
-        OsStr::new("h"),
-        OsStr::new("p"),
-        OsStr::new("i"),
-        OsStr::new("f"),
-        OsStr::new("pas"),
-        OsStr::new("ftn"),
-        OsStr::new("ins.c"),
-        OsStr::new("ins.pas"),
-        OsStr::new("ins.ftn"),
-        OsStr::new("sml"),
-    ]
-});
-
-pub fn choose_random_fortune_file(
-    path: &PathBuf,
-    include_offensive: bool,
-    rng: &mut impl Rand,
-) -> Result<PathBuf, io::Error> {
-    //Couple of rules when it comes to fortune files (defaults)
-    //1. Default fortunes are in /usr/share/games/fortunes
-    //2. Offensive fortunes are in /usr/share/games/fortunes/off. There is no prefix
-    //3. Currently, we load the entire text into memory, so we don't care about DAT
-    //4. Ignore Illegal suffixes in the ILLEGAL_SUFFIXES list
-
-    fn iterate(path: &PathBuf, include_offensive: bool) -> Vec<PathBuf> {
-        let mut total_list = vec![];
-        let dir_list = fs::read_dir(path).expect("Could not open directory");
-        for entry in dir_list.filter(|item| {
-            !ILLEGAL_FILE_SUFFIXES.contains(
-                &item
-                    .as_ref()
-                    .unwrap()
-                    .path()
-                    .extension()
-                    .unwrap_or_default(),
-            )
-        }) {
-            match entry {
-                Ok(item) => match item.metadata().unwrap().is_dir() {
-                    true => {
-                        if (item.file_name() != "./off")
-                            || (item.file_name() == "./off" && include_offensive)
-                        {
-                            total_list.append(&mut iterate(&item.path(), include_offensive))
-                        }
-                    }
-                    false => total_list.push(item.path()),
-                },
-                Err(e) => panic!("Could not identify a file in the fortune directory {e}"),
-            }
-        }
-        total_list
-    }
-
-    // let mut rng = thread_rng();
-    let list = iterate(path, include_offensive);
-    let chosen_idx = rng.next_lim_usize(list.len());
-
-    Ok(list[chosen_idx].clone())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ mod cowsay;
 mod fortune;
 mod parser;
 
-use std::{fs::File, io::Read, path::PathBuf};
-
 use cli::Options;
 #[cfg(not(feature = "inline-cowsay"))]
 use cowsay::identify_cow_path;
 use cowsay::{choose_random_cow, print_cowsay, SpeechBubble};
-#[cfg(feature = "inline-fortune")]
-use fortune::get_inline_fortune;
-use fortune::{choose_fortune_file, get_fortune};
+#[cfg(not(feature = "inline-cowsay"))]
+use std::{fs::File, io::Read};
 use tinyrand::{Seeded, StdRand};
 
 fn main() {
@@ -22,51 +19,39 @@ fn main() {
 
     let options: Options = argh::from_env();
 
-    let cow_str = match &options {
-        options if options.cow_file.is_some() => {
-            match File::open(options.cow_file.as_deref().unwrap()) {
-                Ok(mut file) => {
-                    let mut cow_str = String::new();
-                    file.read_to_string(&mut cow_str)
-                        .expect("Error reading cow string");
-                    cow_str
+    cfg_if::cfg_if! {
+        if #[cfg(feature="inline-cowsay")]{
+            let cow_str = choose_random_cow(&mut rng);
+        } else {
+            let cow_str = match &options.cow_file {
+                 Some(file_path)=> {
+                    match File::open(file_path) {
+                        Ok(mut file) => {
+                            let mut cow_str = String::new();
+                            file.read_to_string(&mut cow_str)
+                                .expect("Error reading Cowfile");
+                            cow_str
+                        }
+                        Err(e) => panic!("{e}"),
+                    }
                 }
-                Err(e) => panic!("{e}"),
-            }
-        }
-        options if options.cow_path.is_some() => choose_random_cow(
-            &Some(PathBuf::from(options.cow_path.as_deref().unwrap())),
-            &mut rng,
-        ),
-        _ => {
-            cfg_if::cfg_if! {
-                if #[cfg(feature="inline-cowsay")]{
-                    choose_random_cow(&None, &mut rng)
-                } else {
-                    let cow_path = identify_cow_path();
-                    choose_random_cow(&Some(cow_path), &mut rng)
-
+                None => {
+                    let cow_path = identify_cow_path(&options.cow_path);
+                    choose_random_cow(&cow_path, &mut rng)
                 }
-            }
-        }
-    };
+            };
+    }};
 
     let cow_msg = match options.message {
         Some(msg) => msg,
         None => {
             cfg_if::cfg_if! {
                 if #[cfg(feature="inline-fortune")]{
-                    if let Some(path) = options.fortune_file{
-                        let fortune_file = choose_fortune_file(options.include_offensive, &mut rng, Some(path) );
-                        get_fortune(fortune_file, &mut rng)
-                            .expect("Could not get a fortune, your future is shrouded in mystery...")
-                    } else {
-                        get_inline_fortune(&mut rng, options.include_offensive)
+                        fortune::get_inline_fortune(&mut rng, options.include_offensive)
                             .expect("Could not read internal fortune index, your future is shrouded in mystery...")
-                    }
                 } else {
-                    let fortune_file = choose_fortune_file(options.include_offensive, &mut rng, options.fortune_file );
-                    get_fortune(fortune_file, &mut rng)
+                    let fortune_file = fortune::choose_fortune_file(options.include_offensive, &mut rng, options.fortune_file );
+                    fortune::get_fortune(fortune_file, &mut rng)
                 .expect("Could not get a fortune, your future is shrouded in mystery...")
                 }
             }
