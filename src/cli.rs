ec361dbd9040cd92ba7d3b62e125530559343250
Alex Westerman <alex@faceftw.dev>
2024-09-06 18:33:39 -0400

    Update Help Text

--- src/cli.rs
+++ src/cli.rs
@@ -7,21 +7,17 @@ use crate::cowsay::{BubbleType, CowVariant};
 pub(crate) struct Options {
     // #[cfg(not(feature = "inline-cowsay"))]
     #[argh(option, short = 'c')]
-    ///path to a direct cowfile
+    ///path to a direct cowfile OR the name of a cow that exists in the cow path
     pub cow_file: Option<String>,
 
     #[cfg(not(feature = "inline-cowsay"))]
     #[argh(option)]
-    ///path to a folder containing multiple cows we should search.
+    ///path to a folder containing cows that shell-toy should use.
     pub cow_path: Option<String>,
 
     #[argh(switch, short = 'l', long = "list-cows")]
     /// lists the cows that are embedded in the executable
     pub list_cows: bool,
-
-    #[argh(switch, short = 'o')]
-    /// whether to include offensive fortunes
-    pub include_offensive: bool,
 
     #[argh(
         option,
@@ -46,13 +42,17 @@ pub(crate) struct Options {
     /// This only affects cowfiles like the default cowsay cow which use the $eyes and/or $toungue variable
     pub cow_variant: CowVariant,
 
-    #[argh(positional)]
-    pub message: Option<String>,
-
     #[cfg(not(feature = "inline-fortune"))]
     #[argh(option, short = 'f', long = "fortune-file")]
     ///instead of using internal fortunes, which file/dir to look in
     pub fortune_file: Option<String>,
+
+    #[argh(switch, short = 'o')]
+    /// whether to include offensive fortunes
+    pub include_offensive: bool,
+
+    #[argh(positional)]
+    pub message: Option<String>,
 }
 
 fn parse_bubble_type(value: &str) -> Result<BubbleType, String> {
