dcf7ad4735128e35db8bc8d65d5aaa686f5f7bf8
Alex Westerman <alex@faceftw.dev>
2026-05-06 23:42:17 -0400

    A bunch of fooling around in adjusting the cli logic

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, ffi::OsStr, fs, io, path::PathBuf};
+
 use argh::FromArgs;
 
 use crate::cowsay::{BubbleType, CowVariant};
@@ -7,14 +9,11 @@ use crate::cowsay::{BubbleType, CowVariant};
 pub(crate) struct Options {
     // #[cfg(not(feature = "inline-cowsay"))]
     #[argh(option, short = 'c')]
-    ///path to a direct cowfile OR the name of a cow that exists in the cow path
-    pub cow_file: Option<String>,
+    /// path to a direct cowfile, a folder containing cow files, or the
+    /// name of a cow file in the COW_PATH if set. Can be repeated to search through multiple paths
+    pub cows: Vec<String>,
 
-    #[cfg(not(feature = "inline-cowsay"))]
-    #[argh(option)]
-    ///path to a folder containing cows that shell-toy should use.
-    pub cow_path: Option<String>,
-
+    #[cfg(feature = "inline-cowsay")]
     #[argh(switch, short = 'l', long = "list-cows")]
     /// lists the cows that are embedded in the executable
     pub list_cows: bool,
@@ -42,10 +41,10 @@ pub(crate) struct Options {
     /// This only affects cowfiles like the default cowsay cow which use the $eyes and/or $toungue variable
     pub cow_variant: CowVariant,
 
-    #[cfg(not(feature = "inline-fortune"))]
-    #[argh(option, short = 'f', long = "fortune-file")]
-    ///instead of using internal fortunes, which file/dir to look in
-    pub fortune_file: Option<String>,
+    #[argh(option, short = 'f', long = "fortunes")]
+    /// what files/folders to look at for fetching fortunes. Multiple paths
+    /// can be provided to expand the list
+    pub fortunes: Vec<String>,
 
     #[argh(option, long = "max-fort-width")]
     /// limits the chosen fortunes to be a maximum number of characters per line
@@ -65,6 +64,148 @@ pub(crate) struct Options {
 
     #[argh(positional)]
     pub message: Option<String>,
+}
+
+macro_rules! illegal_file_suffixes {
+    ($($ext:literal),*) => {
+        [
+            $(std::ffi::OsStr::new($ext)),*
+        ]
+    };
+}
+
+impl Options {
+    pub fn post_init(opts: Options) -> Options {
+        // Cascade all options except for cow and fortunes which
+        // should instead populate the individual files to operate on.
+        Options {
+            cows: match opts.cows.len() {
+                0 => {
+                    cfg_select! {
+                        feature = "inline-cowsay" => { Vec::new() }
+                        not(feature = "inline-cowsay") => { Vec::new() }
+                    }
+                }
+                1 => {
+                    cfg_select! {
+                        feature = "inline-cowsay" => { opts.cows }
+                        not(feature = "inline-cowsay") => { enumerate_files(&PathBuf::from(&opts.cows[0]), Some(&OsStr::new("cow")), None).expect("Could not enumerate cow files in specified path") }
+                    }
+                }
+                _ => opts
+                    .cows
+                    .into_iter()
+                    .flat_map(|path| {
+                        enumerate_files(&PathBuf::from(path), Some(&OsStr::new("cow")), None)
+                            .expect("Could not open some of the files listed")
+                    })
+                    .collect(),
+            },
+            fortunes: match opts.fortunes.len() {
+                0 => cfg_select! {
+                    feature = "inline-fortune" => {
+                        Vec::new()
+                    }
+                    not(feature = "inline-fortune") => {
+                        if let Ok(val) = std::env::var("FORTUNE_FILE") {
+                            vec![val]
+                        } else if let Ok(val) = std::env::var("FORTUNE_PATH") {
+                            enumerate_files(
+                                &PathBuf::from(val),
+                                None,
+                                Some(&illegal_file_suffixes!(
+                                    "dat", "pos", "c", "h", "p", "i", "f", "pas", "ftn", "ins.c",
+                                    "ins.pas", "ins.ftn", "sml", "sh", "pl", "csv"
+                                )),
+                            )
+                            .expect("Could not open some of the files in the FORTUNE_PATH")
+                        } else {
+                            match std::env::consts::OS {
+                                "linux" =>
+                                    enumerate_files(
+                                        &PathBuf::from("/usr/share/games/fortunes"),
+                                        None,
+                                        Some(&illegal_file_suffixes!(
+                                            "dat", "pos", "c", "h", "p", "i", "f", "pas", "ftn", "ins.c",
+                                            "ins.pas", "ins.ftn", "sml", "sh", "pl", "csv"
+                                        )),
+                                    )
+                                    .expect("Could not open some of the files in the default fortunes directory")
+                                _ => panic!(
+                                    "I don't know what the default path for fortunes are for this OS!.\nPlease provide a FORTUNEPATH or FORTUNE_PATH environment variable, or a single file with FORTUNE_FILE"
+                                ),
+                            }
+                        }
+                    }
+                },
+                _ => opts
+                    .fortunes
+                    .into_iter()
+                    .flat_map(|path| {
+                        enumerate_files(
+                            &PathBuf::from(path),
+                            None,
+                            Some(&illegal_file_suffixes!(
+                                "dat", "pos", "c", "h", "p", "i", "f", "pas", "ftn", "ins.c",
+                                "ins.pas", "ins.ftn", "sml", "sh", "pl", "csv"
+                            )),
+                        )
+                        .expect("Could not open some of the files listed")
+                    })
+                    .collect(),
+            },
+            ..opts
+        }
+    }
+}
+
+fn enumerate_files(
+    path: &PathBuf,
+    extension: Option<&OsStr>,
+    excluded_exts: Option<&[&OsStr]>,
+) -> Result<Vec<String>, io::Error> {
+    let mut total_list = vec![];
+    match fs::metadata(path)?.file_type() {
+        //TODO non-unicode handling
+        ft if ft.is_file() => Ok(vec![path.to_string_lossy().to_string()]),
+        ft if ft.is_dir() => {
+            let dir_list = fs::read_dir(path)?.filter(|item| match excluded_exts {
+                Some(excludes) => excludes.contains(
+                    &item
+                        .as_ref()
+                        .unwrap()
+                        .path()
+                        .extension()
+                        .unwrap_or_default(),
+                ),
+                None => true,
+            });
+
+            for entry in dir_list {
+                match entry {
+                    Ok(item) => match item.metadata()?.is_dir() {
+                        true => total_list.append(
+                            enumerate_files(&item.path(), extension, excluded_exts)
+                                .unwrap()
+                                .as_mut(),
+                        ),
+                        false => match extension {
+                            Some(ext) => {
+                                if item.path().extension().unwrap() == ext {
+                                    total_list.push(item.path().to_str().unwrap().to_string());
+                                }
+                            }
+                            None => total_list.push(item.path().to_str().unwrap().to_string()),
+                        },
+                    },
+                    Err(e) => return Err(e),
+                }
+            }
+            Ok(total_list)
+        }
+        //TODO probably don't want to panic, but it does the job
+        _ => panic!("Encountered path {path:?} which was not a file or directory!"),
+    }
 }
 
 fn parse_bubble_type(value: &str) -> Result<BubbleType, String> {
